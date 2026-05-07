c24703b35c4ce0d3f63f3ded83e943782af67179
MohamedAfrasDev <mohamedafrasai@gmail.com>
2026-05-07 15:10:59 +0530

    About Section Updated

--- app/sections/whats-sentraea/What-Is-Sentraea.tsx
+++ app/sections/whats-sentraea/What-Is-Sentraea.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import HeroDiagram from '../Hero/components/hero-diagram'
 import GridCard from './components/grid-card'
+import WorkspaceUi from "@/public/icons/WORKSPACE-UI.svg";
+import WorkspaceUIDARK from "@/public/icons/WORKSPACE-UI-DARK.svg"
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 const WhatIsSentraea = () => {
+    const { theme } = useTheme();
     return (
         <div className='flex flex-col gap-1'>
             <h2 className='text-5xl font-medium tracking-tighter'>What is SENTRAEA?</h2>
-            <h3 className='text-2xl mt-2 tracking-tight'>Sentraea helps turn experience into usable work through four practical modes.</h3>
+            <h3 className='text-xl text-muted-foreground mt-2 tracking-tight'>Sentraea helps turn experience into usable work through four practical modes.</h3>
 
-            <div className='grid grid-cols-2 gap-5'>
-                <GridCard title={'Clarify '} desc={'organize what you know'} />
-                <GridCard title={'Teach '} desc={'explain it clearly'} />
-                <GridCard title={'Solve '} desc={'apply it to problems'} />
-                <GridCard title={'Build '} desc={'turn it into outputs'} />
+            <div className='flex flex-col md:flex-row gap-5 items-start mt-3'>
+                <div className='grid grid-cols-2 h-full gap-4 flex-2 mt-4 '>
+                    <GridCard title={'Clarify '} desc={'Sort what you know.'} isFocal={true} />
+                    <GridCard title={'Teach '} desc={'Explain it simply.'} isFocal={false} />
+                    <GridCard title={'Solve '} desc={'Apply it to real problems.'} isFocal={false} />
+                    <GridCard title={'Build '} desc={'Turn it into useful output.'} isFocal={false} />
+                </div>
+                <div className='text-start md:-mt-6'>
+                    <p className='text-md tracking-tighter text-left'>INSIDE WORKSPACE</p>
+                    <Image
+                        src={theme === "dark" ? WorkspaceUIDARK : WorkspaceUi}
+                        alt="Workspace UI"
+                        width={580}
+                        height={580}
+                        className=''
+                    />
+                </div>
+
             </div>
         </div>
     )
--- app/sections/whats-sentraea/components/grid-card.tsx
+++ app/sections/whats-sentraea/components/grid-card.tsx
@@ -1,15 +1,17 @@
 import { Card } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
 import React from 'react'
 interface GridCardProps {
     title: string;
     desc: string;
+    isFocal?: boolean
 
 }
-const GridCard: React.FC<GridCardProps> = ({ title, desc }) => {
+const GridCard: React.FC<GridCardProps> = ({ title, desc, isFocal }) => {
     return (
-        <Card className='flex flex-col gap-2 px-4 ring-0 shadow-sm'>
-            <h2 className='text-3xl font-medium tracking-tighter'>{title}</h2>
-            <h3 className='text-2xl mt-2 tracking-tight'>{desc}</h3>
+        <Card className={cn('flex flex-col h-full gap-0 px-5 py-5 ring-0  ', isFocal && 'bg-primary/5 dark:bg-primary/10 border dark:border-primary/10')}>
+            <h2 className='text-3xl font-medium tracking-tighter uppercase'>{title}</h2>
+            <h3 className='text-lg mt-1 tracking-tight'>{desc}</h3>
         </Card>
     )
 }
